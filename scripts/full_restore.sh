cc427792cbd717c33e78d6db0a7446daef025f43
Sean G Wilbur <sgwilbur@gmail.com>
2016-03-31 14:22:51 -0500

    Adding comments on exact changes to file.

--- scripts/full_restore.sh
+++ scripts/full_restore.sh
@@ -30,25 +30,47 @@ do
   sudo rsync -avz --delete ${source}/${cur_dir} ${dest}
 done
 
+## Config updates
+## /opt/ibm-ucd/server/conf/server/installed.properties
+
+# hibernate.connection.url=jdbc\:mysql\://uc1\:3306/ibm_ucd
+# hibernate.connection.username=ibm_uc
+# hibernate.connection.password=pbe{KFaeen9jelj6cUBKznHbXaaWzCRnCRHdBfViGcGemxQ\=}
+
+# install.server.web.host=uc1.prod
+# server.external.web.url=https\://uc1\:443
+
+## After startup
+## System Settings
+# Home > Settings > System
+# External Agent URL
+# External User URL
+# Disable email if enabled
+# Accessible via /rest/system/configuration
+# https://www.ibm.com/support/knowledgecenter/SS4GSP_6.1.0/com.ibm.udeploy.api.doc/topics/rest_cli_systemconfiguration.html
+
+
 ## UCR
 dest=/opt/IBM/UCRelease/ucrelease
 source=${data_dir}/release/conf
 
 sudo rsync -avz --delete ${source} ${dest}
 
-# Update 
+# Update
 ucr_config=/opt/IBM/UCRelease/ucrelease/conf/installed.properties
-# public.url
-# hibernate.connection.url=
-# hibernate.connection.username=
-# hibernate.connection.password=
-# license.server.url=
+
+# public.url=https\://uc1.prod\:8443/
+# hibernate.connection.password=pbe{flRLbyNjNV/8Z+ty0ViKSgXtc9AAfxKB9K7k1Q1oBXY\=}
+# hibernate.connection.url=jdbc\:mysql\://localhost\:3306/ibm_ucr
+# hibernate.connection.username=ibm_uc
+# license.server.url=27000@localhost
 # may be turn off email in staging ?
 # mail.smtp.host=
 
+# update in database
+
+read -p "Press [Enter] when you are ready to restart the servers..."
 
 # Start the servers
 sudo /opt/ibm-ucd/server/bin/server start
 sudo /opt/IBM/UCRelease/server/server.startup
- 
-
